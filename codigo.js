c55d8efe049387b6e7f5a8629e29d4890d0d4070
Oswaldo Luis Sierra Bran <oswaldox199@gmail.com>
2021-02-12 00:35:28 -0500

    Reparando problema de video de fondo

--- codigo.js
+++
@@ -1,4 +0,0 @@
-// $(document).ready(function(){
-//     var altura = $(".menu").offset().top;
-//     alert(altura)
-// });
---
+++ js/codigo.js
@@ -0,0 +1,4 @@
+// $(document).ready(function(){
+//     var altura = $(".menu").offset().top;
+//     alert(altura)
+// });
